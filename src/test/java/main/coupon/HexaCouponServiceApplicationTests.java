f237b8359b258e8395a90a878626703e84c1058d
highsiverr <ye90202@gmail.com>
2024-12-09 15:12:56 +0900

    프로젝트 생성

---
+++ src/test/java/main/coupon/HexaCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package main.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaCouponServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
