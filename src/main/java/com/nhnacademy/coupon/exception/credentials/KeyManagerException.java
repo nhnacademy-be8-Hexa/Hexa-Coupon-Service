16b38d778c583d77c878332cb64bb13f7568d90d
highsiverr <ye90202@gmail.com>
2024-12-19 14:39:18 +0900

    feat: 환경분리, db 설정

---
+++ src/main/java/com/nhnacademy/coupon/exception/credentials/KeyManagerException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.coupon.exception.credentials;
+
+public class KeyManagerException extends RuntimeException {
+    public KeyManagerException(String message) {
+        super(message);
+    }
+}
