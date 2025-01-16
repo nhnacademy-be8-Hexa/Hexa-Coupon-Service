988bb886e6359eed0a61e615639936dfb97e99bc
highsiverr <ye90202@gmail.com>
2025-01-16 16:24:40 +0900

    test: 테스트 코드 작성

--- src/main/java/com/nhnacademy/coupon/controller/ApplicationStatusController.java
+++ src/main/java/com/nhnacademy/coupon/controller/ApplicationStatusController.java
@@ -1,17 +1,5 @@
 package com.nhnacademy.coupon.controller;
 
-/*
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2024. NHN Academy Corp. All rights reserved.
- * + * While every precaution has been taken in the preparation of this resource,  assumes no
- * + responsibility for errors or omissions, or for damages resulting from the use of the information
- * + contained herein
- * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
- * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
- * + prior written permission.
- * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- */
-
 import com.netflix.appinfo.ApplicationInfoManager;
 import com.netflix.appinfo.InstanceInfo;
 import com.nhnacademy.coupon.actuator.ApplicationStatus;
@@ -21,10 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author : marco@nhnacademy.com
- * @Date : 27/06/2023
- */
 
 @RestController
 @RequestMapping("/actuator/status")
